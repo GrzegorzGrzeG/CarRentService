1e0f2ad46dda8d808df5f826b3bd9b4438172ad5
Grzegorz Grzegorczyk <g.grzsgorczyk@gmail.com>
2024-10-19 22:54:48 +0200

    Initial commit

---
+++ src/main/java/gg/proj/carrentservice/CarRentServiceApplication.java
@@ -0,0 +1,13 @@
+package gg.proj.carrentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gg/proj/carrentservice/CarRentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.proj.carrentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
