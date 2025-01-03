d106beaa9f36570309f2540c2f4b067e2d1ca180
Grzegorz Grzegorczyk <g.grzsgorczyk@gmail.com>
2025-01-03 20:21:21 +0100

    dodanie styli dla wszystkich podstron, dodanie strony głównej

---
+++ src/main/java/gg/proj/carrentservice/controller/MainController.java
@@ -0,0 +1,17 @@
+package gg.proj.carrentservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "/html/index";
+    }
+}
